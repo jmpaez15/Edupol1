5e1ad05f91bc192ca7038fe0bb8dc1f36c6e8438
Operaciones-GerenteC <Operaciones-GerenteC@10.10.30.182>
2015-09-18 16:20:34 -0500

    Merge origin/master
    Conflicts:
    	web/Estudiantes/Registro.html
    	web/js/Registro.js

--- src/java/com/ethos/business/general/RegistroDatosCodeudor.java
+++ src/java/com/ethos/business/general/RegistroDatosCodeudor.java
@@ -14,4 +14,7 @@ import com.ethos.DAO.AbstractDAO;
 public class RegistroDatosCodeudor {
     AbstractDAO AcudienteDAO;
     AbstractDAO telefonoDAO;
+    AbstractDAO dirreccionDAO;
+    AbstractDAO personaDAO;
+    
 }
